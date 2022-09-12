034c8151c2c4174d9e8e7017cd5dc55c74cd6aa8
Kaida Kohki <kkohki@c1r29s7.42tokyo.jp>
2022-09-12 17:26:20 +0900

    pipe

--- test.c
+++ test.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define READ 0
 #define WRITE 1
 
-int popen(int *fd_r,int *fd_w)
+int popen2(int *fd_r,int *fd_w)
 {
 	int pipe_p2c[2];
 	int pipe_c2p[2];
@@ -12,22 +13,20 @@ int popen(int *fd_r,int *fd_w)
 	pid_t pid;
 	if (pipe(pipe_c2p) < 0)
 	{
-		perror("popen");
-		close(pipe_c2p[READ]);
-		close(pipe_c2p[WRITE]);
+		perror("popen2");
 		return (1);
 	}
 	if (pipe(pipe_p2c) < 0)
 	{
-		perror("popen");
-		close(pipe_p2c[READ]);
-		close(pipe_p2c[WRITE]);
+		perror("popen2");
+		close(pipe_c2p[READ]);
+		close(pipe_c2p[WRITE]);
 		return (1);
 	}
 	//プロセスの作成
 	if ((pid = fork()) < 0)
 	{
-		perror("fork");
+		perror("popen2");
 		close(pipe_c2p[READ]);
 		close(pipe_c2p[WRITE]);
 		close(pipe_p2c[READ]);
@@ -48,26 +47,51 @@ int popen(int *fd_r,int *fd_w)
 		close(pipe_p2c[READ]);
 		close(pipe_c2p[WRITE]);
 
-
+		char	*arg[2];
+		arg[0] = "/bin/cat";
+		arg[1] = NULL;
+		execve("/bin/cat", arg, NULL);
+		perror("popen2");
+		close(pipe_p2c[READ]);
+		close(pipe_c2p[WRITE]);
+		return (1); 
 	}
 
+	close(pipe_p2c[READ]);
+	close(pipe_c2p[WRITE]);
 
+	*fd_r = pipe_c2p[READ];
+	*fd_w = pipe_p2c[WRITE];
 
+	return (pid);
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-	const int buf_size = 256;
-	char buf[buf_size];
+	int fd_r = fileno(stdin);
+	int fd_w = fileno(stdout);
 
-	ssize_t rc;
+	if (argc < 2)
+	{
+		printf("error\n");
+		fflush(stdout);
+		return (0);
+	}
+	popen2(&fd_r, &fd_w);
 
-	rc = read(STDIN_FILENO,buf,buf_size);
-	if (rc == -1)
-		perror("write error");
-	
-	printf("test: ");
-	fwrite(buf,1,rc,stdout);
-	//printf("\n");
+	write(fd_w, argv[1], strlen(argv[1]));
+
+	char buf[255];
+	int size = read(fd_r, buf, 255);
+	if (size == -1)
+	{
+		perror("error");
+		return (1);
+	}
+	printf("test2: ");
+	fwrite(buf, 1, size, stdout);
+	printf("\n");
 	return (0);
 }
+
+
